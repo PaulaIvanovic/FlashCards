0260cb47f0e36ff2925bb4d22a995de82d3fa309
ljovic <joviclucia01@gmail.com>
2024-03-13 20:44:08 +0100

    Spremanje na novu granu

--- HelloGit/src/HelloGit/HelloGit.java
+++ HelloGit/src/HelloGit/HelloGit.java
@@ -3,5 +3,6 @@ package HelloGit;
 public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("Hello, git!");
+		System.out.println("Pokusavam spremit na novu granu");
 	}
 }
