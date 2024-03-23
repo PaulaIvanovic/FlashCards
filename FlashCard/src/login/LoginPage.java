d02042f914857a2e9448f5c698556af87f630842
Mia2404 <miazu@LegionMia>
2024-03-23 16:07:52 +0100

    in all text fields added message the message is deleted when the field is clicked

--- FlashCard/src/login/LoginPage.java
+++ FlashCard/src/login/LoginPage.java
@@ -1,127 +1,192 @@
 package login;
 
-import java.awt.EventQueue;
-import java.awt.Font;
-import java.awt.Image;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import java.awt.Color;
-import javax.swing.JTextField;
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JPasswordField;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.border.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 
 public class LoginPage extends JFrame {
 
-	private static final long serialVersionUID = 1L;
-	private JPanel contentPane;
-	private JTextField user;
-	private JPasswordField pass;
+    private static final long serialVersionUID = 1L;
+    private JPanel contentPane;
+    private JTextField user;
+    private JPasswordField pass;
+    private JLabel passPlaceholder; 
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-			//ako postoji frame zatvori ga
-				
-				try {
-					LoginPage frame = new LoginPage();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    LoginPage frame = new LoginPage();
+                    frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
 
-	/**
-	 * Create the frame.
-	 */
-	public LoginPage() {
-		setTitle("REGISTER PAGE");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 800, 530);
-		contentPane = new JPanel();
-		contentPane.setBackground(new Color(69, 62, 130));
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+    public LoginPage() {
+        setTitle("REGISTER PAGE");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(100, 100, 800, 530);
+        contentPane = new JPanel();
+        contentPane.setBackground(new Color(69, 62, 130));
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        setContentPane(contentPane);
+        contentPane.setLayout(null);
+        
+        JPanel panel_1 = new JPanel();
+        panel_1.setBounds(188, 29, 496, 464);
+        contentPane.add(panel_1);
+        panel_1.setBackground(new Color(69, 62, 130));
+        panel_1.setLayout(null);
+        
+        JLabel usernameText = new JLabel("Username:");
+        usernameText.setFont(new Font("Tahoma", Font.BOLD, 12));
+        usernameText.setForeground(Color.WHITE);
+        usernameText.setBounds(82, 161, 160, 25);
+        panel_1.add(usernameText);
+        
+        JLabel textpass = new JLabel("Password:");
+        textpass.setFont(new Font("Tahoma", Font.BOLD, 12));
+        textpass.setForeground(Color.WHITE);
+        textpass.setBounds(82, 224, 85, 13);
+        panel_1.add(textpass);
+        
+        JLabel lblNewLabel_2 = new JLabel("Already have an account?");
+        lblNewLabel_2.setForeground(new Color(255, 0, 0));
+        lblNewLabel_2.setBounds(47, 396, 205, 13);
+        panel_1.add(lblNewLabel_2);
+        
+        JButton loginBtn = new JButton("Login");
+        loginBtn.setForeground(Color.BLACK);
+        loginBtn.setFont(new Font("Tahoma", Font.BOLD, 12));
+        loginBtn.setBounds(283, 330, 85, 21);
+        panel_1.add(loginBtn);
+        
+        JButton registerBtn = new JButton("Register");
+        registerBtn.setForeground(Color.BLACK);
+        registerBtn.setFont(new Font("Tahoma", Font.BOLD, 12));
+        registerBtn.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e) {
+                RegisterPage register = new RegisterPage();
+                register.setVisible(true);
+                dispose();
+            }
+        });
+        registerBtn.setBounds(283, 388, 85, 21);
+        panel_1.add(registerBtn);
+        
+        user = new JTextField("Enter your name");
+        user.setColumns(10);
+        user.setBounds(82, 195, 273, 19);
+        user.addFocusListener(new FocusListener() {
+            public void focusGained(FocusEvent e) {
+                if (user.getText().equals("Enter your name")) {
+                    user.setText(""); // Briše tekst samo ako je "Upiši svoje ime" prisutno
+                }
+            }
 
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
-		
-		JPanel panel_1 = new JPanel();
-		panel_1.setBounds(188, 29, 496, 464);
-		contentPane.add(panel_1);
-		panel_1.setBackground(new Color(69, 62, 130));
-		panel_1.setLayout(null);
-		
-		JLabel usernameText = new JLabel("Username:");
-		usernameText.setFont(new Font("Tahoma", Font.BOLD, 12));
-		usernameText.setForeground(Color.WHITE);
-		usernameText.setBounds(82, 161, 160, 25);
-		panel_1.add(usernameText);
-		
-		JLabel textpass = new JLabel("Password:");
-		textpass.setFont(new Font("Tahoma", Font.BOLD, 12));
-		textpass.setForeground(Color.WHITE);
-		textpass.setBounds(82, 224, 85, 13);
-		panel_1.add(textpass);
-		
-		JLabel lblNewLabel_2 = new JLabel("Already have an account?");
-		lblNewLabel_2.setForeground(new Color(255, 0, 0));
-		lblNewLabel_2.setBounds(47, 396, 205, 13);
-		panel_1.add(lblNewLabel_2);
-		
-		//funkcionalnost login buttona
-		JButton loginBtn = new JButton("Login");
-		loginBtn.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				LoginPage login = new LoginPage();
-				login.setVisible(true);
-				dispose();
-			}
-		});
-		loginBtn.setBounds(283, 330, 85, 21);
-		panel_1.add(loginBtn);
-		
-		JButton registerBtn = new JButton("Register");
-		registerBtn.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent e) {
-				RegisterPage register = new RegisterPage();
-				register.setVisible(true);
-				dispose();
-			}
-		});
-		registerBtn.setBounds(283, 388, 85, 21);
-		panel_1.add(registerBtn);
-		
-		user = new JTextField();
-		user.setColumns(10);
-		user.setBounds(82, 195, 273, 19);
-		panel_1.add(user);
-		
-		pass = new JPasswordField();
-		pass.setBounds(82, 252, 273, 19);
-		panel_1.add(pass);
-		
-		JLabel lblNewLabel = new JLabel("FLASH CARDS");
-		lblNewLabel.setForeground(new Color(255, 255, 255));
-		lblNewLabel.setFont(new Font("Nirmala UI", Font.BOLD, 54));
-		lblNewLabel.setBounds(112, 30, 374, 81);
-		panel_1.add(lblNewLabel);
-		
-		JLabel photoLabel = new JLabel("");
-		Image IMG = new ImageIcon(this.getClass().getResource("/logo.png")).getImage().getScaledInstance(90, 90,Image.SCALE_DEFAULT);;
-		photoLabel.setIcon(new ImageIcon(IMG));
-		photoLabel.setBounds(10, 30, 78, 81);
-		panel_1.add(photoLabel);
-	}
+            public void focusLost(FocusEvent e) {
+                if (user.getText().isEmpty()) {
+                    user.setText("Enter your name"); // Postavlja tekst samo ako nema unosa
+                }
+            }
+        });
+        panel_1.add(user);
+        
+        pass = new JPasswordField();
+        pass.setBounds(82, 252, 273, 19);
+        pass.setEchoChar('\u2022');
+        panel_1.add(pass);
+
+        // Dodano: labela za prikaz teksta prije unosa lozinke
+        passPlaceholder = new JLabel("Enter your password");
+        passPlaceholder.setForeground(Color.GRAY);
+        passPlaceholder.setBounds(82, 252, 273, 19);
+        panel_1.add(passPlaceholder); // Dodajemo labelu u panel
+
+        // Postavljanje teksta "Enter your password" u polju za unos lozinke
+        pass.setEchoChar((char) 0); // Omogućuje prikazivanje teksta umjesto zvjezdica
+        pass.setText("Enter your password");
+
+        pass.addFocusListener(new FocusListener() {
+            public void focusGained(FocusEvent e) {
+                pass.setEchoChar('\u2022'); // Postavljanje natrag zvjezdica prilikom fokusa
+                if (new String(pass.getPassword()).equals("Enter your password")) {
+                    pass.setText(""); // Briše tekst samo ako je "Enter your password" prisutno
+                }
+                passPlaceholder.setVisible(false); // Sakriva labelu kada je polje za unos lozinke fokusirano
+            }
+
+            public void focusLost(FocusEvent e) {
+                if (pass.getPassword().length == 0) {
+                    pass.setEchoChar((char) 0); // Omogućuje prikazivanje teksta umjesto zvjezdica
+                    pass.setText("Enter your password"); // Postavljanje teksta samo ako nema unosa
+                    passPlaceholder.setVisible(true); // Ponovno prikazuje labelu ako nema unosa u polju za lozinku
+                }
+            }
+        });
+
+        pass.getDocument().addDocumentListener(new DocumentListener() {
+            public void changedUpdate(DocumentEvent e) {
+                updatePasswordVisibility();
+            }
+
+            public void removeUpdate(DocumentEvent e) {
+                updatePasswordVisibility();
+            }
+
+            public void insertUpdate(DocumentEvent e) {
+                updatePasswordVisibility();
+            }
+
+            private void updatePasswordVisibility() {
+                if (new String(pass.getPassword()).isEmpty()) {
+                    passPlaceholder.setVisible(true);
+                } else {
+                    passPlaceholder.setVisible(false);
+                }
+            }
+        });
+
+
+        JLabel lblNewLabel = new JLabel("FLASH CARDS");
+        lblNewLabel.setForeground(new Color(255, 255, 255));
+        lblNewLabel.setFont(new Font("Nirmala UI", Font.BOLD, 54));
+        lblNewLabel.setBounds(112, 30, 374, 81);
+        panel_1.add(lblNewLabel);
+        
+        JLabel photoLabel = new JLabel("");
+        photoLabel.setBounds(10, 30, 78, 81);
+        panel_1.add(photoLabel);
+        
+        //rounded corners on button
+        loginBtn.setBorder(new RoundedBorder(20));
+        registerBtn.setBorder(new RoundedBorder(20));
+    }
+    
+    //class for rounded buttons
+    class RoundedBorder implements Border {
+        private int radius;
+
+        RoundedBorder(int radius) {
+            this.radius = radius;
+        }
+
+        public Insets getBorderInsets(Component c) {
+            return new Insets(this.radius+1, this.radius+1, this.radius+2, this.radius);
+        }
+
+        public boolean isBorderOpaque() {
+            return true;
+        }
+
+        public void paintBorder(Component c, Graphics g, int x, int y, int width, int height) {
+            g.drawRoundRect(x, y, width-1, height-1, radius, radius);
+        }
+    }
 }
--- FlashCard/src/login/RegisterPage.java
+++ FlashCard/src/login/RegisterPage.java
@@ -4,6 +4,9 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.LineBorder;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+
 import java.awt.Color;
 import java.awt.GridLayout;
 import java.awt.Image;
@@ -13,6 +16,9 @@ import javax.swing.ImageIcon;
 import java.awt.SystemColor;
 import java.awt.Font;
 import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import java.awt.geom.RoundRectangle2D;
 import java.awt.event.ActionEvent;
 import java.awt.Dimension;
 import javax.swing.SwingConstants;
@@ -39,12 +45,12 @@ import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JTextPane;
 
-public class RegisterPage extends JFrame {
+public class RegisterPage extends JFrame implements Template{
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	private JTextField user;
-	private JTextField textField_3;
+	private JTextField emailField;
 	private JPasswordField pass;
 
 	/**
@@ -76,29 +82,33 @@ public class RegisterPage extends JFrame {
 		contentPane.setLayout(null);
 		
 		JPanel panel_1 = new JPanel();
-		panel_1.setBounds(188, 29, 496, 464);
+		panel_1.setBounds(203, 29, 496, 464);
 		contentPane.add(panel_1);
 		panel_1.setBackground(new Color(69, 62, 130));
 		panel_1.setLayout(null);
 		
+		//username:
 		JLabel usernameText = new JLabel("Username:");
 		usernameText.setFont(new Font("Tahoma", Font.BOLD, 12));
 		usernameText.setForeground(Color.WHITE);
 		usernameText.setBounds(82, 161, 160, 25);
 		panel_1.add(usernameText);
 		
+		
+		//password:
 		JLabel textpass = new JLabel("Password:");
 		textpass.setFont(new Font("Tahoma", Font.BOLD, 12));
 		textpass.setForeground(Color.WHITE);
 		textpass.setBounds(82, 276, 85, 13);
 		panel_1.add(textpass);
 		
+		//message Already have an account?
 		JLabel lblNewLabel_2 = new JLabel("Already have an account?");
 		lblNewLabel_2.setForeground(new Color(255, 0, 0));
 		lblNewLabel_2.setBounds(47, 396, 205, 13);
 		panel_1.add(lblNewLabel_2);
 		
-		//funkcionalnost login buttona
+		//login button function
 		JButton loginBtn = new JButton("Login");
 		loginBtn.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -107,9 +117,12 @@ public class RegisterPage extends JFrame {
 				dispose();
 			}
 		});
-		loginBtn.setBounds(284, 392, 85, 21);
+		loginBtn.setForeground(Color.BLACK);
+		loginBtn.setFont(secFont);
+		loginBtn.setBounds(270, 392, 85, 21);
 		panel_1.add(loginBtn);
 		
+		//email:
 		JLabel emailText = new JLabel("Email:");
 		emailText.setFont(new Font("Tahoma", Font.BOLD, 12));
 		emailText.setForeground(Color.WHITE);
@@ -117,23 +130,118 @@ public class RegisterPage extends JFrame {
 		emailText.setBounds(82, 224, 45, 13);
 		panel_1.add(emailText);
 		
-		JButton registerBtn = new JButton("Register");
-		registerBtn.setBounds(284, 339, 85, 21);
-		panel_1.add(registerBtn);
-		
 		user = new JTextField();
 		user.setColumns(10);
 		user.setBounds(82, 195, 273, 19);
 		panel_1.add(user);
 		
-		textField_3 = new JTextField();
-		textField_3.setColumns(10);
-		textField_3.setBounds(82, 247, 273, 19);
-		panel_1.add(textField_3);
+		JLabel userPlaceholder = new JLabel("Enter your username");
+		userPlaceholder.setForeground(Color.GRAY);
+		userPlaceholder.setBounds(82, 195, 273, 19);
+		panel_1.add(userPlaceholder); // Dodajemo labelu u panel
+
+		// Postavljanje teksta "Enter your username" u polje za unos korisničkog imena
+		user.setText("Enter your username");
+
+		user.addFocusListener(new FocusListener() {
+		    public void focusGained(FocusEvent e) {
+		        if (user.getText().equals("Enter your username")) {
+		            user.setText(""); // Briše tekst samo ako je "Enter your username" prisutno
+		        }
+		        userPlaceholder.setVisible(false); // Sakriva labelu kada je polje za unos korisničkog imena fokusirano
+		    }
+
+		    public void focusLost(FocusEvent e) {
+		        if (user.getText().isEmpty()) {
+		            user.setText("Enter your username"); // Postavljanje teksta samo ako nema unosa
+		            userPlaceholder.setVisible(true); // Ponovno prikazuje labelu ako nema unosa u polju za korisničko ime
+		        }
+		    }
+		});
+		
+		emailField = new JTextField();
+		emailField.setColumns(10);
+		emailField.setBounds(82, 247, 273, 19);
+		panel_1.add(emailField);
+		
+		JLabel emailPlaceholder = new JLabel("Enter your email");
+		emailPlaceholder.setForeground(Color.GRAY);
+		emailPlaceholder.setBounds(82, 247, 273, 19);
+		panel_1.add(emailPlaceholder); 
+
+		//text "Enter your email" in textfield
+		emailField.setText("Enter your email");
+
+		emailField.addFocusListener(new FocusListener() {
+		    public void focusGained(FocusEvent e) {
+		        if (emailField.getText().equals("Enter your email")) {
+		            emailField.setText("");
+		        }
+		        emailPlaceholder.setVisible(false); 
+		    }
+
+		    public void focusLost(FocusEvent e) {
+		        if (emailField.getText().isEmpty()) {
+		            emailField.setText("Enter your email");
+		            emailPlaceholder.setVisible(true); 
+		        }
+		    }
+		});
 		
 		pass = new JPasswordField();
 		pass.setBounds(82, 299, 273, 19);
 		panel_1.add(pass);
+
+		JLabel passPlaceholder = new JLabel("Enter your password");
+		passPlaceholder.setForeground(Color.GRAY);
+		passPlaceholder.setBounds(82, 299, 273, 19);
+		panel_1.add(passPlaceholder); 
+		
+		// text "Enter your password" in textfield
+		pass.setEchoChar((char) 0); 
+		pass.setText("Enter your password");
+
+		pass.addFocusListener(new FocusListener() {
+		    public void focusGained(FocusEvent e) {
+		        pass.setEchoChar('\u2022'); 
+		        if (new String(pass.getPassword()).equals("Enter your password")) {
+		            pass.setText(""); 
+		        }
+		        passPlaceholder.setVisible(false); 
+		    }
+
+		    public void focusLost(FocusEvent e) {
+		        if (pass.getPassword().length == 0) {
+		            pass.setEchoChar((char) 0);
+		            pass.setText("Enter your password"); 
+		            passPlaceholder.setVisible(true);
+		        }
+		    }
+		});
+
+		pass.getDocument().addDocumentListener(new DocumentListener() {
+		    public void changedUpdate(DocumentEvent e) {
+		        updatePasswordVisibility();
+		    }
+
+		    public void removeUpdate(DocumentEvent e) {
+		        updatePasswordVisibility();
+		    }
+
+		    public void insertUpdate(DocumentEvent e) {
+		        updatePasswordVisibility();
+		    }
+
+		    private void updatePasswordVisibility() {
+		        if (new String(pass.getPassword()).isEmpty()) {
+		            passPlaceholder.setVisible(true);
+		        } else {
+		            passPlaceholder.setVisible(false);
+		        }
+		    }
+		});
+
+		
 		
 		JLabel lblNewLabel = new JLabel("FLASH CARDS");
 		lblNewLabel.setForeground(new Color(255, 255, 255));
@@ -141,11 +249,31 @@ public class RegisterPage extends JFrame {
 		lblNewLabel.setBounds(112, 30, 374, 81);
 		panel_1.add(lblNewLabel);
 		
+		//added logo photo
 		JLabel photoLabel = new JLabel("");
 		Image IMG = new ImageIcon(this.getClass().getResource("/logo.png")).getImage().getScaledInstance(90, 90,Image.SCALE_DEFAULT);
 		photoLabel.setIcon(new ImageIcon(IMG));
 		photoLabel.setBounds(10, 30, 78, 81);
 		panel_1.add(photoLabel);
+		
+		//message this fields can not be empty
+		JLabel lblNewLabel_3 = new JLabel("* this fields can not be empty");
+		lblNewLabel_3.setBounds(82, 330, 192, 25);
+		panel_1.add(lblNewLabel_3);
+		lblNewLabel_3.setFont(new Font("Dialog", Font.PLAIN, 12));
+		lblNewLabel_3.setForeground(textRed);
+		
+		//register button function
+		JButton registerBtn = new JButton("Register");
+		registerBtn.setBounds(270, 361, 85, 21);
+		panel_1.add(registerBtn);
+		registerBtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		
+		registerBtn.setForeground(Color.BLACK);
+		registerBtn.setFont(new Font("Dialog", Font.PLAIN, 12));
 	}
 	}
 
